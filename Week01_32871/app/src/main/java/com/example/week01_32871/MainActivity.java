dbb035bf65fe29988379157c24c2dfca7a37fbc2
omgede <64190947+omgede@users.noreply.github.com>
2021-02-02 09:57:05 +0700

    Modul pertama ganti hello world jadi nama dan nim

---
+++ Week01_32871/app/src/main/java/com/example/week01_32871/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.week01_32871;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.appcompat.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
